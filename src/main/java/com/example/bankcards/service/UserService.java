b1070a0daf87129e8fc50bb2d33b7244254d3f61
Maksim Zhigarev <zigarevmaksim9@gmail.com>
2026-08-23 17:05:31 +0300

    feat: create exception for invalid credentials

--- src/main/java/com/example/bankcards/service/UserService.java
+++ src/main/java/com/example/bankcards/service/UserService.java
@@ -4,6 +4,7 @@ import com.example.bankcards.dto.RegisterRequestDTO;
 import com.example.bankcards.dto.RegisterResponseDTO;
 import com.example.bankcards.entity.User;
 import com.example.bankcards.entity.enums.Role;
+import com.example.bankcards.exception.InvalidCredentialsException;
 import com.example.bankcards.exception.UsernameAlreadyExistsException;
 import com.example.bankcards.repository.UserRepository;
 import com.example.bankcards.security.JwtService;
