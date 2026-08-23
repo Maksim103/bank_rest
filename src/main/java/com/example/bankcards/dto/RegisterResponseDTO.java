fdc7bf750342de8ce4ceadb7f1dbc1a969c172e7
Maksim Zhigarev <zigarevmaksim9@gmail.com>
2026-08-23 13:10:10 +0300

    feat: create registration response DTO

---
+++ src/main/java/com/example/bankcards/dto/RegisterResponseDTO.java
@@ -0,0 +1,18 @@
+package com.example.bankcards.dto;
+
+import com.example.bankcards.entity.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterResponseDTO {
+
+    private Long id;
+    private String username;
+    private Role role;
+}
