1c66a032512605e7d2014fc1960d3a56346906a2
Maksim Zhigarev <zigarevmaksim9@gmail.com>
2026-08-23 13:13:53 +0300

    feat: create user registration service

---
+++ src/main/java/com/example/bankcards/service/UserService.java
@@ -0,0 +1,40 @@
+package com.example.bankcards.service;
+
+import com.example.bankcards.dto.RegisterRequestDTO;
+import com.example.bankcards.dto.RegisterResponseDTO;
+import com.example.bankcards.entity.User;
+import com.example.bankcards.entity.enums.Role;
+import com.example.bankcards.exception.UsernameAlreadyExistsException;
+import com.example.bankcards.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public RegisterResponseDTO registerUser(RegisterRequestDTO registerRequestDTO) {
+        String username = registerRequestDTO.getUsername();
+        String password = registerRequestDTO.getPassword();
+
+        if (userRepository.existsUserByUsername(username)) {
+            throw new UsernameAlreadyExistsException("Username already exists");
+        }
+
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(passwordEncoder.encode(password));
+        user.setRole(Role.USER);
+
+        User savedUser = userRepository.save(user);
+
+        return new RegisterResponseDTO(savedUser.getId(), savedUser.getUsername(), savedUser.getRole());
+    }
+}
