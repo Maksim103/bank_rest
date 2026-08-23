e85b6c5790fc04ac2fee4a876a00cb85144b3eff
Maksim Zhigarev <zigarevmaksim9@gmail.com>
2026-08-23 13:09:25 +0300

    feat: create registration request DTO

---
+++ src/main/java/com/example/bankcards/dto/RegisterRequestDTO.java
@@ -0,0 +1,24 @@
+package com.example.bankcards.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequestDTO {
+
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters long")
+    @NotBlank(message = "Username cannot be blank")
+    private String username;
+
+    @Size(min = 8, max = 50, message = "Password must be between 8 and 50 characters long")
+    @NotBlank(message = "Password cannot be blank")
+    private String password;
+
+}
