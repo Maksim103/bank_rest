ff61d82fdabdbd65e542061f579a3bdf5e0ea28e
Maksim Zhigarev <zigarevmaksim9@gmail.com>
2026-08-31 20:47:24 +0300

    feat: add validation constraints for username and password

--- src/main/java/com/example/bankcards/dto/login/LoginRequestDTO.java
+++ src/main/java/com/example/bankcards/dto/login/LoginRequestDTO.java
@@ -1,6 +1,7 @@
 package com.example.bankcards.dto.login;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,9 +12,12 @@ import lombok.Setter;
 @Getter
 @Setter
 public class LoginRequestDTO {
+
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters long")
     @NotBlank(message = "Username cannot be blank")
     private String username;
 
+    @Size(min = 8, max = 50, message = "Password must be between 8 and 50 characters long")
     @NotBlank(message = "Password cannot be blank")
     private String password;
 }
