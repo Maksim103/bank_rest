da9eabad278a9be33a05d9a94ca4be394f65382e
Maksim Zhigarev <zigarevmaksim9@gmail.com>
2026-08-31 20:47:08 +0300

    feat: implement token validation and extraction methods in JwtService

--- src/main/java/com/example/bankcards/security/jwt/JwtService.java
+++ src/main/java/com/example/bankcards/security/jwt/JwtService.java
@@ -1,9 +1,11 @@
-package com.example.bankcards.security;
+package com.example.bankcards.security.jwt;
 
 import com.example.bankcards.entity.enums.Role;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import javax.crypto.SecretKey;
 import java.util.Date;
@@ -28,6 +30,30 @@ public class JwtService {
                 .expiration(new Date(System.currentTimeMillis() + jwtExpiration))
                 .signWith(getSigningKey(), Jwts.SIG.HS256)
                 .compact();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        String name = extractUsername(token);
+        return (name != null && name.equals(userDetails.getUsername())
+                && !isTokenExpired(token));
+    }
+
+    public String extractUsername(String token) {
+        return extractAllClaims(token).get("username", String.class);
+    }
+
+    private boolean isTokenExpired(String token) {
+        Date exp = extractAllClaims(token).get("exp", Date.class);
+
+        return exp.before(new Date());
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
     }
 
     private SecretKey getSigningKey() {
