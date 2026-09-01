817a71f24c5908264d360da0f307c121fdd647ba
Maksim Zhigarev <zigarevmaksim9@gmail.com>
2026-09-01 19:30:47 +0300

    feat: create CardResponseDTO for representing card details in responses

---
+++ src/main/java/com/example/bankcards/dto/CardResponseDTO.java
@@ -0,0 +1,25 @@
+package com.example.bankcards.dto;
+
+import com.example.bankcards.entity.enums.CardStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.YearMonth;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class CardResponseDTO {
+
+    private Long id;
+    private Long ownerId;
+    private String cardNumber;
+    private YearMonth expirationDate;
+    private CardStatus cardStatus;
+    private BigDecimal balance;
+
+}
