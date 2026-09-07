e98e1609e014165d3251e5fde53520280491aeee
Maksim Zhigarev <zigarevmaksim9@gmail.com>
2026-09-07 21:06:43 +0300

    feat: create CardNotFoundException for handling card not found scenarios

---
+++ src/main/java/com/example/bankcards/exception/CardNotFoundException.java
@@ -0,0 +1,11 @@
+package com.example.bankcards.exception;
+
+public class CardNotFoundException extends RuntimeException {
+    public CardNotFoundException() {
+      super();
+    }
+
+    public CardNotFoundException(String message) {
+          super(message);
+      }
+}
