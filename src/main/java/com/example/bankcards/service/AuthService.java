bf365f7c8a67d95956c7ffe45f71c8ff0262571a
Maksim Zhigarev <zigarevmaksim9@gmail.com>
2026-09-08 14:33:41 +0300

    feat: implement AuthService for user registration and login functionality

---
+++ src/main/java/com/example/bankcards/service/AuthService.java
@@ -0,0 +1,62 @@
+package com.example.bankcards.service;
+
+import com.example.bankcards.dto.login.LoginRequestDTO;
+import com.example.bankcards.dto.login.LoginResponseDTO;
+import com.example.bankcards.dto.register.RegisterRequestDTO;
+import com.example.bankcards.dto.register.RegisterResponseDTO;
+import com.example.bankcards.entity.User;
+import com.example.bankcards.entity.enums.Role;
+import com.example.bankcards.exception.InvalidCredentialsException;
+import com.example.bankcards.exception.UsernameAlreadyExistsException;
+import com.example.bankcards.repository.UserRepository;
+import com.example.bankcards.security.jwt.JwtService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtService jwtService) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
+    }
+
+    public RegisterResponseDTO registerUser(RegisterRequestDTO registerRequestDTO) {
+        String username = registerRequestDTO.getUsername();
+        String password = registerRequestDTO.getPassword();
+
+        if (userRepository.existsUserByUsername(username)) {
+            throw new UsernameAlreadyExistsException("Username already exists");
+        }
+
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(passwordEncoder.encode(password));
+        user.setRole(Role.USER);
+
+        User savedUser = userRepository.save(user);
+
+        return new RegisterResponseDTO(savedUser.getId(), savedUser.getUsername(), savedUser.getRole());
+    }
+
+    public LoginResponseDTO loginUser(LoginRequestDTO loginRequestDTO) {
+        String username = loginRequestDTO.getUsername();
+        String password = loginRequestDTO.getPassword();
+
+        User user = userRepository.findUserByUsername(username)
+                .orElseThrow(() -> new InvalidCredentialsException("Invalid username or password"));
+
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            throw new InvalidCredentialsException("Invalid username or password");
+        }
+
+        String token = jwtService.generateToken(user.getId(), user.getUsername(), user.getRole());
+
+        return new LoginResponseDTO(token);
+    }
+}
