fa56a2209488d71dc98bec85c5d1fdb39dbbd988
Maksim Zhigarev <zigarevmaksim9@gmail.com>
2026-09-08 14:35:44 +0300

    feat: extend UserRepository with role-based user counting and pagination support

--- src/main/java/com/example/bankcards/repository/UserRepository.java
+++ src/main/java/com/example/bankcards/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package com.example.bankcards.repository;
 
 import com.example.bankcards.entity.User;
+import com.example.bankcards.entity.enums.Role;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,4 +11,9 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByUsername(String username);
     boolean existsUserByUsername(String username);
+    long countUserByRole(Role role);
+    boolean existsUserByIdAndCardsNotEmpty(Long id);
+
+    @Override
+    Page<User> findAll(Pageable pageable);
 }
