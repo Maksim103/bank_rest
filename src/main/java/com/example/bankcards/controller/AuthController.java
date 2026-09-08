e9104fbf1e2b89d042fa720b8f2138506ec9f4cc
Maksim Zhigarev <zigarevmaksim9@gmail.com>
2026-09-08 14:37:53 +0300

    fix: update AuthController to use AuthService instead of UserService

--- src/main/java/com/example/bankcards/controller/AuthController.java
+++ src/main/java/com/example/bankcards/controller/AuthController.java
@@ -4,7 +4,7 @@ import com.example.bankcards.dto.login.LoginRequestDTO;
 import com.example.bankcards.dto.login.LoginResponseDTO;
 import com.example.bankcards.dto.register.RegisterRequestDTO;
 import com.example.bankcards.dto.register.RegisterResponseDTO;
-import com.example.bankcards.service.UserService;
+import com.example.bankcards.service.AuthService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,21 +17,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class AuthController {
 
-    private final UserService userService;
+    private final AuthService authService;
 
-    public AuthController(UserService userService) {
-        this.userService = userService;
+    public AuthController(AuthService authService) {
+        this.authService = authService;
     }
 
     @PostMapping("/register")
     public ResponseEntity<RegisterResponseDTO> registerUser(@RequestBody @Valid RegisterRequestDTO registerRequestDTO) {
-        RegisterResponseDTO responseDTO = userService.registerUser(registerRequestDTO);
+        RegisterResponseDTO responseDTO = authService.registerUser(registerRequestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
     }
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDTO> loginUser(@RequestBody @Valid LoginRequestDTO loginRequestDTO) {
-        LoginResponseDTO response = userService.loginUser(loginRequestDTO);
+        LoginResponseDTO response = authService.loginUser(loginRequestDTO);
         return ResponseEntity.ok(response);
     }
 }
