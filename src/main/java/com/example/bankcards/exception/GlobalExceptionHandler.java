36ca0300892400bb348a96cff084a656b5d30891
Maksim Zhigarev <zigarevmaksim9@gmail.com>
2026-09-11 23:02:57 +0300

    feat: add exception handlers for BlockRequest and Card exceptions in GlobalExceptionHandler

--- src/main/java/com/example/bankcards/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/bankcards/exception/GlobalExceptionHandler.java
@@ -1,4 +1,10 @@
 package com.example.bankcards.exception;
+import com.example.bankcards.exception.blockRequest.BlockRequestAlreadyExistsException;
+import com.example.bankcards.exception.blockRequest.BlockRequestNotFoundException;
+import com.example.bankcards.exception.card.CardNotActiveException;
+import com.example.bankcards.exception.card.CardNotFoundException;
+import com.example.bankcards.exception.user.UserNotFoundException;
+import com.example.bankcards.exception.user.UsernameAlreadyExistsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -37,6 +43,33 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(CardNotFoundException.class)
     public ResponseEntity<ErrorResponseDTO> handleCardNotFoundException(CardNotFoundException ex) {
+        ErrorResponseDTO errorResponse = new ErrorResponseDTO(
+                HttpStatus.NOT_FOUND.value(),
+                ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
+    @ExceptionHandler(CardNotActiveException.class)
+    public ResponseEntity<ErrorResponseDTO> handleCardNotActiveException(CardNotActiveException ex) {
+        ErrorResponseDTO errorResponse = new ErrorResponseDTO(
+                HttpStatus.BAD_REQUEST.value(),
+                ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+    @ExceptionHandler(BlockRequestAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponseDTO> handleBlockRequestAlreadyExistsException(BlockRequestAlreadyExistsException ex) {
+        ErrorResponseDTO errorResponse = new ErrorResponseDTO(
+                HttpStatus.CONFLICT.value(),
+                ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
+    }
+
+    @ExceptionHandler(BlockRequestNotFoundException.class)
+    public ResponseEntity<ErrorResponseDTO> handleBlockRequestNotFoundException(BlockRequestNotFoundException ex) {
         ErrorResponseDTO errorResponse = new ErrorResponseDTO(
                 HttpStatus.NOT_FOUND.value(),
                 ex.getMessage());
