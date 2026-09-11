5b9acdfd5f214044fbd59bd4c4fff0da5d2536f8
Maksim Zhigarev <zigarevmaksim9@gmail.com>
2026-09-11 23:13:10 +0300

    feat: update CardAdminController to use CardAdminService for card management operations

--- src/main/java/com/example/bankcards/controller/CardAdminController.java
+++ src/main/java/com/example/bankcards/controller/CardAdminController.java
@@ -2,7 +2,7 @@ package com.example.bankcards.controller;
 
 import com.example.bankcards.dto.card.CardRequestDTO;
 import com.example.bankcards.dto.card.CardResponseDTO;
-import com.example.bankcards.service.CardService;
+import com.example.bankcards.service.card.CardAdminService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,31 +12,31 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/admin/cards")
 public class CardAdminController {
 
-    private final CardService cardService;
+    private final CardAdminService cardAdminService;
 
-    public CardAdminController(CardService cardService) {
-        this.cardService = cardService;
+    public CardAdminController(CardAdminService cardAdminService) {
+        this.cardAdminService = cardAdminService;
     }
 
     @PostMapping
     public ResponseEntity<CardResponseDTO> createCard(@RequestBody @Valid CardRequestDTO cardRequestDTO) {
-        CardResponseDTO responseDTO = cardService.createCard(cardRequestDTO.getOwnerId());
+        CardResponseDTO responseDTO = cardAdminService.createCard(cardRequestDTO.getOwnerId());
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
     }
 
     @PutMapping("/block/{cardId}")
     public ResponseEntity<CardResponseDTO> blockCard(@PathVariable Long cardId) {
-        return ResponseEntity.ok(cardService.blockCard(cardId));
+        return ResponseEntity.ok(cardAdminService.blockCard(cardId));
     }
 
     @PutMapping("/activate/{cardId}")
     public ResponseEntity<CardResponseDTO> activateCard(@PathVariable Long cardId) {
-        return ResponseEntity.ok(cardService.activateCard(cardId));
+        return ResponseEntity.ok(cardAdminService.activateCard(cardId));
     }
 
-    @DeleteMapping("/delete/{cardId}")
+    @DeleteMapping("/{cardId}")
     public ResponseEntity<Void> deleteCard(@PathVariable Long cardId) {
-        cardService.deleteCard(cardId);
+        cardAdminService.deleteCard(cardId);
         return ResponseEntity.noContent().build();
     }
 }
